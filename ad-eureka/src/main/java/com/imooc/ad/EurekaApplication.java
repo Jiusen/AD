796d6e293d7d2a0a85ce80c15e6658fd05635a72
Jiusen <2434732570@qq.com>
2021-01-31 02:02:24 +0800

    Eureka Server服务的部署(多点/集群)

--- ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
+++ ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -14,6 +14,6 @@ public class EurekaApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(EnableEurekaServer.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
