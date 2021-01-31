29d681263a9b3baec87631311c8b87af415c21c2
Jiusen <2434732570@qq.com>
2021-02-01 00:39:22 +0800

    通用模块-统一异常处理的开发

---
+++ imooc-ad-service/ad-common/src/main/java/com/imooc/ad/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,26 @@
+package com.imooc.ad.advice;
+
+import com.imooc.ad.exception.AdException;
+import com.imooc.ad.vo.CommonResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/2/1 0:27
+ * 全局异常处理类
+ */
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    //异常处理方法，只处理AdException异常
+    @ExceptionHandler(value = AdException.class)
+    public CommonResponse<String> handlerAdException(HttpServletRequest request, AdException ex) {
+        CommonResponse<String> response = new CommonResponse<>(-1, "business error");
+        response.setData(ex.getMessage());
+        return response;
+    }
+
+}
---
+++ imooc-ad-service/ad-common/src/main/java/com/imooc/ad/exception/AdException.java
@@ -0,0 +1,14 @@
+package com.imooc.ad.exception;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/2/1 0:26
+ * 异常处理类
+ */
+public class AdException extends Exception{
+
+    public AdException(String message) {
+        super(message);
+    }
+
+}
