13cb924130a5c09e02d0b4e99f00dcbc9ae7beea
Jiusen <2434732570@qq.com>
2021-02-01 16:37:59 +0800

    广告投放系统-创建(子模块)

---
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/SponsorApplication.java
@@ -0,0 +1,22 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/2/1 16:34
+ * 主启动
+ */
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableFeignClients
+@SpringBootApplication
+public class SponsorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class, args);
+    }
+}
