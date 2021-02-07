eb3ae14a908cc16520ee3d59d4a4ebdaba67c021
Jiusen <2434732570@qq.com>
2021-02-07 22:05:33 +0800

    兴趣索引对象定义与服务实现

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/interest/UnitItObject.java
@@ -0,0 +1,18 @@
+package com.imooc.ad.index.interest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/2/7 21:57
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitItObject {
+
+    private Long unitId;
+    private String itTag;
+}
