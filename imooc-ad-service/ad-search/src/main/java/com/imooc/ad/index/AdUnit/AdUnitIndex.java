a9ee58a28cf2d43bc39fe95c7e3166aab9a5c65c
Jiusen <2434732570@qq.com>
2021-02-07 20:41:05 +0800

    推广单元索引对象定义与服务实现

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/AdUnit/AdUnitIndex.java
@@ -0,0 +1,61 @@
+package com.imooc.ad.index.AdUnit;
+
+import com.imooc.ad.index.IndexAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/2/7 20:34
+ */
+@Slf4j
+@Component
+public class AdUnitIndex implements IndexAware<Long, AdUnitObject> {
+
+    private static Map<Long, AdUnitObject> objectMap;
+
+    static {
+        objectMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public AdUnitObject get(Long key) {
+        return objectMap.get(key);
+    }
+
+    @Override
+    public void add(Long key, AdUnitObject value) {
+
+        log.info("before add: {}", objectMap);
+        objectMap.put(key, value);
+        log.info("after add: {}", objectMap);
+    }
+
+    @Override
+    public void update(Long key, AdUnitObject value) {
+
+        log.info("before update: {}", objectMap);
+
+        //null直接插入 or 更新（自定义查看）
+        AdUnitObject oldObject = objectMap.get(key);
+        if (null == oldObject) {
+            objectMap.put(key, value);
+        } else {
+            oldObject.update(value);
+        }
+
+        log.info("after update: {}", objectMap);
+    }
+
+    @Override
+    public void delete(Long key, AdUnitObject value) {
+
+        log.info("before delete: {}", objectMap);
+        objectMap.remove(key);
+        log.info("after delete: {}", objectMap);
+    }
+}
---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/AdUnit/AdUnitObject.java
@@ -0,0 +1,42 @@
+package com.imooc.ad.index.AdUnit;
+
+import com.imooc.ad.index.adplan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Jiusen Guo
+ * @date 2021/2/7 20:28
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdUnitObject {
+
+    private Long unitId;
+    private Integer unitStatus;
+    private Integer positionType;
+    private Long planId;
+
+    private AdPlanObject adPlanObject;
+
+    void update(AdUnitObject newObject) {
+
+        if (null != newObject.getUnitId()) {
+            this.unitId = newObject.getUnitId();
+        }
+        if (null != newObject.getUnitStatus()) {
+            this.unitStatus = newObject.getUnitStatus();
+        }
+        if (null != newObject.getPositionType()) {
+            this.positionType = newObject.getPositionType();
+        }
+        if (null != planId) {
+            this.planId = newObject.getPlanId();
+        }
+        if (null != newObject.getAdPlanObject()) {
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+    }
+}
