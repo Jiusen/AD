bcb473083f311dec2f855922a21cf75b5f3db36a
Jiusen <2434732570@qq.com>
2021-02-07 22:31:04 +0800

    地域索引对象定义与服务实现

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/utils/CommonUtils.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/utils/CommonUtils.java
@@ -1,5 +1,13 @@
 package com.imooc.ad.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -7,6 +15,7 @@ import java.util.function.Supplier;
  * @author Jiusen Guo
  * @date 2021/2/7 21:00
  */
+@Slf4j
 public class CommonUtils {
 
     /**
@@ -20,5 +29,34 @@ public class CommonUtils {
      */
     public static <K, V> V getorCreate(K key, Map<K, V> map, Supplier<V> factory) {
         return map.computeIfAbsent(key, k -> factory.get());
+    }
+
+    //拼接
+    public static String stringConcat(String... args) {
+
+        StringBuilder result = new StringBuilder();
+        for (String arg : args) {
+            result.append(arg);
+            result.append("-");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    // Tue Jan 01 08:00:00 CST 2019
+    public static Date parseStringDate(String dateString) {
+
+        try {
+
+            DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+            return DateUtils.addHours(
+                    dateFormat.parse(dateString),
+                    -8
+            );
+
+        } catch (ParseException ex) {
+            log.error("parseStringDate error: {}", dateString);
+            return null;
+        }
     }
 }
