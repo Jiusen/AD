68ca7aa85a122f420510ac2f44892617f709edd4
Jiusen <2434732570@qq.com>
2021-02-14 02:34:52 +0800

    检索服务响应对象的定义

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/search/ISearch.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/search/ISearch.java
@@ -1,9 +1,18 @@
 package com.imooc.ad.search;
 
 
+import com.imooc.ad.search.vo.SearchRequest;
+import com.imooc.ad.search.vo.SearchResponse;
+
 /**
- * Created by Qinyi.
+ * 搜索服务接口
  */
 public interface ISearch {
 
+    /**
+     * 搜索服务
+     * @param request 请求数据
+     * @return  返回应答
+     */
+    SearchResponse fetchAds(SearchRequest request);
 }
